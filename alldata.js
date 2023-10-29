7b76a15e6d90df1663f3d966e305deb62509ac47
Nicholas Usey <94800866+NickUsey@users.noreply.github.com>
2023-10-29 16:53:30 -0500

    Update alldata.js
    launching the all data page

--- alldata.js
+++ alldata.js
@@ -1,9 +1,40 @@
-function AllData(){
-  const ctx = React.useContext(UserContext);
+function AllData() {
+  const [userSubmissions, setUserSubmissions] = React.useState([]);
+  
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+
+        const response = await fetch('https://api.example.com/user-submissions');
+        const data = await response.json();
+        setUserSubmissions(data);
+      } catch (error) {
+        console.error('Error fetching user submissions:', error);
+      }
+    };
+    
+    fetchData();
+  }, []);
+
   return (
-    <>
-    <h5>All Data in Store</h5>
-    {JSON.stringify(ctx)}<br/>
-    </>
+    <div className="col-md-5 mb-3">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">All Data</h5>
+          <ul>
+            {userSubmissions.map((submission, index) => (
+              <li key={index}>
+                {/* Display user submission data here */}
+                <p>Name: {submission.name}</p>
+                <p>Email: {submission.email}</p>
+                {/* Add more fields as needed */}
+                <hr />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
