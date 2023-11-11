9ce7f1bad46abc0ba7070970d15300d25ed0d0b5
Nicholas Usey <94800866+NickUsey@users.noreply.github.com>
2023-11-11 17:13:41 -0600

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { UserContext } from './Context'; 
+import { UserContext } from './context.js'; 
 
 function Deposit() {
   const ctx = useContext(UserContext);
