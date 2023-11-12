a1ed7e5cf7e88ab88f74f89df80d6b750af4647d
Nicholas Usey <94800866+NickUsey@users.noreply.github.com>
2023-11-12 06:00:16 -0600

    Update withdraw.js

--- withdraw.js
+++ withdraw.js
@@ -2,7 +2,7 @@ function Withdraw() {
   const ctx = React.useContext(UserContext);
   
   const [withdrawAmount, setWithdrawAmount] = React.useState('');
-  const [balance, setBalance] = React.useState(ctx.users[].balance);
+  const [balance, setBalance] = React.useState(ctx.users[0].balance);
   const [message, setMessage] = React.useState('');
   const [alert, setAlert] = React.useState('');
 
